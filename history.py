5d8d6836d772f94be46e78c089fd623db95431c0
DH <74167514+tanakatanakajava@users.noreply.github.com>
2020-12-24 11:41:42 +0900

    Add files via upload

---
+++ history.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed Dec  9 11:28:50 2020)---
+runfile('C:/Users/ic201236/.spyder-py3/temp.py', wdir='C:/Users/ic201236/.spyder-py3')
+print("Hello")
+runfile('C:/Users/ic201236/.spyder-py3/temp.py', wdir='C:/Users/ic201236/.spyder-py3')
---
+++ temp.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x=2
+print(x*3)
+
+
+a=100;b=200 ;c=0
+
+a,b=b,a
+
+print(a,b)
+
+a=10
+b=2
+
+a=5;b=3;
+
+print(a%b)
+
+a=5;b=10
+
+print(a**b)
+
+if a>b:
+    print(a)
+    
+else:
+    print(b)
+    
+a=5
+if a%2==0:
+    print(True)
+
+else :
+    print(False)
+    
+    
+    
+
+        
+
+
+
+
+
+
+

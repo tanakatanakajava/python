9e68c1ae93a9fadbb9d977c8f5a999e621a647f8
DH <74167514+tanakatanakajava@users.noreply.github.com>
2020-12-27 16:50:49 +0900

    Add files via upload

---
+++ if.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです。
+"""
+a=10
+b=98
+
+print("a">"b")
+print("a"<"b")
+print("a<b")
+print("python"<"Python")
+#TrueかFalseを表示する
+#小文字＞大文字
+
+a=1
+if a==1:
+    print("正解")
+#コロン（：）を忘れない
+#Tabキーで処理の文
+
+b=8 
+c=3
+if b==8:
+    print("成功")
+
+
+w=8
+q=3
+if w==3:
+    print("if文を実行")
+    
+elif w==8:
+    print("elifを実行")
+
+else:
+    print("elseを実行")
+
+
+
+

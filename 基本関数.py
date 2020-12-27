99d9952e059d519240f0801d926ed427f4f351e0
DH <74167514+tanakatanakajava@users.noreply.github.com>
2020-12-27 11:36:09 +0900

    Add files via upload

---
+++ 基本関数.py
@@ -0,0 +1,34 @@
+x=-200
+
+y=abs(x)
+
+print(y)
+
+value=100*-1
+abs(value) #絶対値を表示
+print(value)
+
+f=123.456
+print(round(f)) #少数を整数に変える
+
+print(min([10,3,4,5,6])) #一番小さい値を表示する
+print(max([10,3,4,5,6])) #一番大きい値を表示する
+
+print(round(f,1))
+print(round(f,2)) #少数の桁数を表示する
+print(round(f,3))
+print(type(round(f,0))) #typeは、型を確認する
+
+
+import math 
+#mathモジュールをインポートする
+#(同じ階層ならファイル形式でも可能)
+num=math.sqrt(2.0) #sqrt→平方根を求める
+print(num)
+
+
+
+
+
+
+
---
+++ 文字列メッソド.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです。
+"""
+
+print("Python Programmming")
+
+input("好きな文字を入力してください")
+#scanfみたいなもん
+input()
+#これもあり
+
+string=input("")
+print("文字列",string,"が入力されました")
+
+text="lower letters"
+uppred_text=text.upper()
+print(uppred_text)
+#upper()は文字列をすべて大文字に変換する
+#lower letters というデータの、upper() というメソッドを呼び出しています
+
+text_a="the sea"
+text_a.find("sea")
+#find()は文字列を検索する関数
+#この場合は、４番目に出てくるため
+#左から０，１，２、３となる
+#データ.メソッド名
+
+"lower letters".upper()
+#メッソド名.関数
+#upper()は、文字列をすべて大文字人変換する
+

13d401c66dc032132e977bc286ede403362262cb
DH <74167514+tanakatanakajava@users.noreply.github.com>
2020-12-28 20:40:02 +0900

    Add files via upload

---
+++ リスト（配列）.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 16:23:57 2020
+
+@author: ic201236
+"""
+
+#リストオブジェクト
+#決まった順番に並んだ値で構成される情報
+
+stations=["東京","品川","横浜"]
+print(stations)
+
+#オブジェクト名＝["要素１","要素2"・・・]
+#文字列の場合
+
+
+cases=[100,200,300]
+print(cases)
+#数字の要素は”ダブルクォーテーションがいらない
+
+empty=[]
+print(empty)
+#からのリスト
+
+print(stations[0])
+
+stations=["東京","品川","横浜"]
+i=0;
+while i<3:
+    print(stations[i])
+    i+=1
+    
+
+    

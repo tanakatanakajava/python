1140d36ec4ae87b3456a3034c009adf62d9ab560
DH <74167514+tanakatanakajava@users.noreply.github.com>
2020-12-31 10:27:58 +0900

    Add files via upload

---
+++ 練習問題１-１.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 31 10:11:41 2020
+
+@author: ic201236
+"""
+
+a=5
+b=2
+
+wa=a+b
+sa=a-b
+seki=a*b
+shou=a//b
+amari=a/b
+# a//bは、商と余りを表示
+# a/bは、商を表示（余りを表示しない）
+
+print(a,"+",b,'=',wa)
+print(a,"-",b,'=',sa)
+print(a,'*',b,'=',seki)
+print(a,"//",b,'=',shou)
+print(a,"/",b,'=',amari)
+
+#実行結果
+#5 + 2 = 7
+#5 - 2 = 3
+#5 * 2 = 10
+#5 // 2 = 2
+#5 / 2 = 2.5
+
+     

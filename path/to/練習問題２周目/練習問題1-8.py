ac2575072c2fb6458a453369c477260902b0d8f1
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-01-07 12:40:51 +0900

    Add files via upload

---
+++ path/to/練習問題２周目/練習問題1-8.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  7 12:22:02 2021
+
+"""
+number=int(input("数＞＞"))
+ans=1
+while 1<=number:
+        
+        ans*=number
+        
+        number-=1
+    
+print(ans)
+    
+  #階乗を表示させる

e8b3dc1b56f1cd5f95dace5a44c7b6c69b5a0afb
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-01-08 12:13:28 +0900

    Add files via upload

---
+++ path/to/練習問題２周目/練習問題1-9.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 12:03:03 2021
+
+"""
+
+value=int(input("数字を入力"))
+
+num=value
+
+while 1<=num:
+    print("■ ",end='')
+    #end''は、改行を削除する
+    num-=1

00333039c6eaf1861c102c3f0dce2fc01a463906
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-01-25 12:10:40 +0900

    Add files via upload

---
+++ path/to/練習問題3/1-1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 10:54:13 2021
+
+"""
+a=5;
+b=2;
+
+print("5+2=%d" %(a+b))
+print("5-2=%d"%(a-b))
+print("5×2=%d"%(a*b))
+print("5/2=%dあまり%d"%(a/b,a%b))
+print("5/2=%f"%(a/b))
+
+
+

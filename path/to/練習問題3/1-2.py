0c96fad84f42354cdd0eadf226f706b4a797b1cf
ndhidhi <74167514+tanakatanakajava@users.noreply.github.com>
2021-01-25 12:13:02 +0900

    Add files via upload

---
+++ path/to/練習問題3/1-2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 11:03:59 2021
+
+"""
+
+eng=int(input("英語の成績を入力してください"))
+
+math=int(input("数学の成績を入力してください"))
+
+print("英語の得点%d"%(eng))
+print("数学の得点%d"%(math))
+
+print("合計%d"%(math+eng))
+print("平均%d"%((math+eng)/2))
